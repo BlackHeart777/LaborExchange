e6d6a8fd0ade00ffb3677383bad5905b706cfd2b
BlackHeart777 <artyom-zap@ya.ru>
2015-11-11 16:59:57 +0300

    Test getignore

--- LaborExchange/DataAccess/Entities/Employer.cs
+++ LaborExchange/DataAccess/Entities/Employer.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DataAccess.Entities
 {
@@ -11,6 +14,8 @@ namespace DataAccess.Entities
         /// <summary>
         /// Employer Id.
         /// </summary>
+        [Key]
+        [Index("IX_FieldIndex_Id", IsUnique = true)]
         public int Id { get; set; }
 
         /// <summary>
